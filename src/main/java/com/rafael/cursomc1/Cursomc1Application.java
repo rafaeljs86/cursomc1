f44a37b1af95264c6c00fa7f77cbb604c48c688d
rafael joaquim <rafaeljoaquim86@gmail.com>
2018-11-10 10:26:41 -0200

    primeiro commit no mac

---
+++ src/main/java/com/rafael/cursomc1/Cursomc1Application.java
@@ -0,0 +1,12 @@
+package com.rafael.cursomc1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cursomc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cursomc1Application.class, args);
+	}
+}
