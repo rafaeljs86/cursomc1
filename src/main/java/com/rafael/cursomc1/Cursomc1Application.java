400b6ec7ecdbbbbdaacf7888475f3ed322b953e5
rafael joaquim <rafaeljoaquim86@gmail.com>
2018-11-10 21:57:30 -0200

    criando instanciação no banco

--- src/main/java/com/rafael/cursomc1/Cursomc1Application.java
+++ src/main/java/com/rafael/cursomc1/Cursomc1Application.java
@@ -1,12 +1,32 @@
 package com.rafael.cursomc1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Cursomc1Application {
+import com.rafael.cursomc1.domain.Categoria;
+import com.rafael.cursomc1.repositories.CategoriaRepositories;
 
+@SpringBootApplication
+public class Cursomc1Application implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepositories categoriaRepository;
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Cursomc1Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Escritório");
+		Categoria cat2 = new Categoria(null, "Farmacia");
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 }
